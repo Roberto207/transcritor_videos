cb804c3e450938cc6dfdbdcd6c1e24ed6c4911ed
Roberto Caetano Neto <robertocaetano207@gmail.com>
2025-08-09 17:28:17 -0300

    Update transcritor.py

--- transcritor.py
+++ transcritor.py
@@ -6,10 +6,11 @@ def transcrever_audio(arquivo_audio):
     return resultado['text']
 
 if __name__ == "__main__":
-    caminho_arquivo = 'C:\\Users\\Windows\\Pictures\\videomulherteste1.mp4' #input("Digite o caminho do arquivo de vídeo ou áudio a ser transcrito: ")
+    caminho_arquivo = 'SEU_CAMINHO_AQUI' #COLOQUE O CAMINHO DO SEU VIDEO #input("Digite o caminho do arquivo de vídeo ou áudio a ser transcrito: ")
     roteiro = transcrever_audio(caminho_arquivo)
 
     print("\n--- Roteiro transcrito ---\n")
     print(roteiro)
-    #print(roteiro)
+   
+
 
